b0ac2b053a965d7814f97f37aac7786e980239c1
Kirill <gusak.ks@yandex.ru>
2022-04-28 12:39:17 +0300

    router

--- src/routes/routeRouter.js
+++ src/routes/routeRouter.js
@@ -1,16 +1,19 @@
 const router = require('express').Router();
 const { Route } = require('../../db/models');
-
+const {User} = require ()
 router.get('/route/:id', async (req, res) => {
   const routeId = req.params.id;
   console.log(routeId);
 
   try {
     const route = await Route.findOne({ where: { id: routeId } });
+    const 
     res.render('info', { route });
   } catch (error) {
     console.log(error);
   }
 });
+
+
 
 module.exports = router;
